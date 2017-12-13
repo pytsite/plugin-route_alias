c7da49b830695739b5278c3898805e3b02a9aebc
Alexander Shepetko <a@shepetko.com>
2017-12-13 18:09:44 +0200

    Release 1.1

--- __init__.py
+++ __init__.py
@@ -1,22 +1,21 @@
-"""Route Alias Package Init
+"""PytSite Route Alias Plugin
 """
-# Public API
-from ._api import create, find, get_by_target, get_by_alias
-from . import _model as model, _error as error
-
-
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
+from pytsite import plugman as _plugman
 
-def _init():
+if _plugman.is_installed(__name__):
+    # Public API
+    from ._api import create, find, get_by_target, get_by_alias
+    from . import _model as model, _error as error
+
+
+def plugin_load():
     from pytsite import router
     from plugins import odm
     from . import _eh
 
     odm.register_model('route_alias', model.RouteAlias)
     router.on_pre_dispatch(_eh.router_pre_dispatch)
-
-
-_init()
